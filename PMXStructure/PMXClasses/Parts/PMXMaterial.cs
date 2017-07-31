b86dd454ea7d78596ef847fdb4b5593558684b51
Link <5476865+haolink@users.noreply.github.com>
2017-07-31 18:34:52 +0200

    Implementing abstract method

--- PMXStructure/PMXClasses/Parts/PMXMaterial.cs
+++ PMXStructure/PMXClasses/Parts/PMXMaterial.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PMXStructure.PMXClasses.Helpers;
 
 namespace PMXStructure.PMXClasses.Parts
 {
@@ -10,6 +12,16 @@ namespace PMXStructure.PMXClasses.Parts
     {
         public PMXMaterial(PMXModel model) : base(model)
         {
+        }
+
+        public override void FinaliseAfterImport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadFromStream(BinaryReader br, MMDImportSettings importSettings)
+        {
+            throw new NotImplementedException();
         }
     }
 }
