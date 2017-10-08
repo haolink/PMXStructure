b490a99a9fa47b9c9cb3ab088cad2351d4d0ba0a
Link <5476865+haolink@users.noreply.github.com>
2017-10-08 09:58:20 +0200

    Assembly Info.

--- PMXLibrary/Properties/AssemblyInfo.cs
+++ PMXLibrary/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PMX Library")]
 [assembly: AssemblyDescription("Library to manage PMD, PMX and VMD files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("haolink")]
 [assembly: AssemblyProduct("PMX Library")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©haolink  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("en-us")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
